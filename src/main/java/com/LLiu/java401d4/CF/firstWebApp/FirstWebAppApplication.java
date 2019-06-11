486fa2dc0f2203d53453b3e7520c2bfa68bb4b05
xialiu1988 <liuxia198896@gmail.com>
2019-06-11 16:07:42 -0700

    initial commit

---
+++ src/main/java/com/LLiu/java401d4/CF/firstWebApp/FirstWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.LLiu.java401d4.CF.firstWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebAppApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(FirstWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LLiu/java401d4/CF/firstWebApp/FirstWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.LLiu.java401d4.CF.firstWebApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
